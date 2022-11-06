be29dbb2ee7cde6bdeec04f6e2534bf4a92404b9
Arezaaa <115351385+Arezaaa@users.noreply.github.com>
2022-11-06 15:39:11 +0800

    Update PRAK403-2210817210016-AHMAD_REZA_ALFAYIET.c

--- MODUL 5/SOAL3/PRAK403-2210817210016-AHMAD_REZA_ALFAYIET.c
+++ MODUL 5/SOAL3/PRAK403-2210817210016-AHMAD_REZA_ALFAYIET.c
@@ -7,8 +7,8 @@ if(a1<a2) {
 for(int i=a1;i<=a2;a1++,a2--) {
     printf("%d %d ", a1, a2);
     if(i<a2) {
-        printf("- ");}}}
-if(a1>a2) {
+        printf("- ");}}
+        } else if(a1>a2) {
 for(int i=a2;i<=a1;a2++,a1--) {
     printf("%d %d ", a1, a2);
     if(i<a1) {

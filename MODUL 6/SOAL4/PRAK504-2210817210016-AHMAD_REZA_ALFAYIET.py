0ad67c545530e088181d40dbf30523738543a594
Arezaaa <115351385+Arezaaa@users.noreply.github.com>
2022-12-02 20:11:27 +0800

    Update PRAK504-2210817210016-AHMAD_REZA_ALFAYIET.py

--- MODUL 6/SOAL4/PRAK504-2210817210016-AHMAD_REZA_ALFAYIET.py
+++ MODUL 6/SOAL4/PRAK504-2210817210016-AHMAD_REZA_ALFAYIET.py
@@ -1,7 +1,10 @@
 def reverse(a):
-    rev=reverse(a)
+    rev=0
+    while a>0:
+        rev=(rev*10)+a%10
+        a=a//10
     return rev;
-A,B=list(map(int,input().split()))
+A,B=map(int,input().split())
 A=reverse(A)
 B=reverse(B)
 C=A+B
